3ec10327b20b2292b10be23fd4656c54a81ae85e
eliorodr2104 <eliorodr0904@gmail.com>
2025-08-10 16:54:58 +0200

    Initial Commit

---
+++ NeonC/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NeonC
+//
+//  Created by Eliomar Alejandro Rodriguez Ferrer on 10/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NeonC/NeonCApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeonCApp.swift
+//  NeonC
+//
+//  Created by Eliomar Alejandro Rodriguez Ferrer on 10/08/25.
+//
+
+import SwiftUI
+
+@main
+struct NeonCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
